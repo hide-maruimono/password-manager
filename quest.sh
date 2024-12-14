8ba376af7432b46f640ef1172b5f5913b5baacc2
root <hideaki.katoooo@gmail.com>
2024-12-14 15:16:50 +0900

    Exit made

--- quest.sh
+++ quest.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 #user interface
-echo "パスワードマネージャーへようこそ!"
-echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
-read option
 
-echo "サービス名を入力してください："
-read service
+    echo "パスワードマネージャーへようこそ!"
+    echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+    read option
 
-echo "ユーザー名を入力してください："
-read user
-
-echo "パスワードを入力してください："
-read password
-
+while [ "$option" != "Exit" ]; do
 # Add Password が入力された場合
-if [ "$option" == "Add Password" ]; then
-echo "サービス名を入力してください："
-read service
+        if [ "$option" == "Add Password" ]; then
+        echo "サービス名を入力してください："
+        read service
 
-echo "ユーザー名を入力してください："
-read user
+        echo "ユーザー名を入力してください："
+        read user
 
-echo "パスワードを入力してください："
-read password
+        echo "パスワードを入力してください："
+        read password
 
-echo "パスワードの追加は成功しました。"
+        echo "$service:$user:$password" >> list.txt
+        echo "パスワードの追加は成功しました。"
 
 # Get Password が入力された場合
-elif [ "$option" == "Get Password" ]; then
-echo "サービス名を入力してください："
-read service
-## サービス名が保存されていなかった場合
-
-echo "そのサービスは登録されていません。"
+    elif [ "$option" == "Get Password" ]; then
+        echo "サービス名を入力してください："
+        read service
 
 ## サービス名が保存されていた場合
-echo "サービス名：hoge"
-echo "ユーザー名：fuga"
-echo "パスワード：piyo"
+        if grep -q "$service" "$list.txt" ; then
+            echo "サービス名：$service"
+            echo "ユーザー名：$user"
+            echo "パスワード：$password"
 
-# Add Password/Get Password/Exit 以外が入力された場合
+
+## サービス名が保存されていなかった場合
+        else
+            echo "そのサービスは登録されていません。"
+        fi
 
 # Exit が入力された場合
-elif [ "$option" == "Exit" ]; then
-echo "Thank you!"
+    elif [ "$option" == "Exit" ]; then
+        echo "Thank you!"
+
 ## プログラムが終了
-echo "$service:$user:$password" >> list.txt
+        break
+
+# Add Password/Get Password/Exit 以外が入力された場合
+    else echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+
+    fi
+done
